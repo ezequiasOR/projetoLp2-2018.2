f4fb7a79b258714520d3e54db3e7a075af67f32f
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2018-11-20 06:56:53 -0300

    Adicionando o Facade

--- src/doe/Facade.java
+++ src/doe/Facade.java
@@ -1,5 +1,9 @@
 package doe;
 
 public class Facade {
-
+	private ControllerUsuarios ctlUsuarios;
+	
+	public void adicionaDoador(int id, String nome, String email, String celular, String classe) {
+		ctlUsuarios.adicionaDoador(id, nome, email, celular, classe);
+	}
 }
