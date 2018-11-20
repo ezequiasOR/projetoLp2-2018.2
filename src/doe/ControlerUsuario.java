081f6b38a3aaa2d529b98c9db3cfd2d050b4fc99
João Vitor de Melo <joao.vitor.souza@ccc.ufcg.edu.br>
2018-11-20 06:49:51 -0300

    commit de teste

---
+++ src/doe/ControlerUsuario.java
@@ -0,0 +1,21 @@
+package doe;
+
+import java.util.ArrayList;
+
+public class ControlerUsuario {
+	
+	private ArrayList<Usuario> usuarios;
+	
+	public ControlerUsuario() {
+		this.usuarios = new ArrayList<>();
+	}
+	
+	public void cadastraUsuario(String nome, String email, String celular, String id) {
+		
+	}
+	
+	public String pesquisaUsuario(String id) {
+		
+	}
+
+}
