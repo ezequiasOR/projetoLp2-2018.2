ba942364ff6e42d54e9e2cd6a95b719972a8add6
118110103 JOAO VITOR DE MELO CAVALCANTE E SOUZA <joao.vitor.souza@ccc.ufcg.edu.br>
2018-11-26 08:45:59 -0300

    corrigindo erros do descritores

--- src/doe/Doador.java
+++ src/doe/Doador.java
@@ -1,16 +1,28 @@
 package doe;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Doador extends Usuario {
+	
 	private String status;
+	private Set<Item> itens;
 
 	public Doador(String id, String nome, String email, String celular, String classe) {
 		super(id, nome, email, celular, classe);
 		status = "doador";
+		this.itens = new HashSet<>();
+	}
+	
+	public void adicionaItem(Item item) {
+		itens.add(item);
 	}
 
 	@Override
 	public String toString() {
 		return String.format("%s/%s, %s, %s, status: %s", this.nome, this.id, this.email, this.celular, this.status);
 	}
+	
+	
 	
 }
